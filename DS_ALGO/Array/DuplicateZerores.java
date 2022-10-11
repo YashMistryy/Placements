cccbb408111ab277386a9000509f41025e7c2d1d
yash mistry <mistryy37@gmail.com>
2022-10-11 17:33:12 +0530

    ././

--- DS_ALGO/Array/DuplicateZerores.java
+++ DS_ALGO/Array/DuplicateZerores.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class DuplicateZerores {
     public static void main(String[] args) {
-        
+        // duplicate zeroes means add a zero after each occurence of 0 in array
+        // keep size of array same
+        // alter the original array
+        int[] nums = {1,4,0,8,0,9};
+        duplicateZeroes(nums);
+    }
+
+    private static void duplicateZeroes(int[] nums) {
+        int[] arr = new int[nums.length];
+        int j = 0;
+        int i = 0;
+
+        while(i<nums.length && j<nums.length) {
+            if(nums[i] != 0){
+                arr[j++] = nums[i];
+                
+            }else{
+                arr[j] = 0;
+                arr[j+1] = 0;
+                j++;
+                 
+            }
+            i++;
+            j++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
