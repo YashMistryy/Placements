9f18659ec06ab036e49e5a8f391359f258bd185a
yash mistry <mistryy37@gmail.com>
2022-10-14 10:06:46 +0530

    ././

--- DS_ALGO/Array/DS_ALGO/Array/RemoveElementFrmArr.java
+++ DS_ALGO/Array/DS_ALGO/Array/RemoveElementFrmArr.java
@@ -6,31 +6,18 @@ public class RemoveElementFrmArr {
     public static void main(String[] args) {
         // remove each occurence of an element from an array
         // then sort the array part which is correct
-         int[] nums = {1,2,3,4,55,5,6,7,55,8,9,11,55,55};
-        reoveElement(nums, 55);
+         int[] nums = {0,1,2,2,3,0,4,2};
+       System.out.println(removeElement(nums, 2));
     }
 
-    private static void reoveElement(int[] nums, int n) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == n)
-            count++;
+    // private static void reoveElement(int[] nums, int n) {
+       static int  removeElement(int[] arr , int val){
+        int j = 0;
+        for(int i:arr){
+            if(i != val)
+            arr[j++] = i;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == n){
-                // count++;
-                for (int j = i+1; j < nums.length; j++) {
-                    nums[j-1] = nums[j];                    
-                }
-                System.out.println(Arrays.toString(nums));
-            }
-
+          System.out.println(Arrays.toString(arr));
+          return j;
         }
-        System.out.println(count);
-        for (int i = 0; i < nums.length-count; i++) {
-            System.out.print(nums[i]+" ");
-        }
-        // System.out.println(Arrays.toString(nums));
-
-    }
 }
