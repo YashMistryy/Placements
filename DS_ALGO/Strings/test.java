b048f2fe61a2432c7d38013fb8d060ce97ef4da7
yash mistry <mistryy37@gmail.com>
2022-10-16 19:20:22 +0530

    .//

---
+++ DS_ALGO/Strings/test.java
@@ -0,0 +1,21 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        // String str = "yash";
+        // String str2 = new String("yash");
+        // System.out.println(str.equals(str2));
+        // // System.out.println(str == str2);
+        // String[] strArr = {"foo" , "baa","baz"};
+        // strArr[1] = "bar";
+        // System.out.println(Arrays.toString(strArr));
+        // String myStr  = "yashmmistry";
+        // myStr.charAt(4) = ' ';
+        for (String str : args) {
+            System.out.println(str);
+        }
+
+    }
+}
