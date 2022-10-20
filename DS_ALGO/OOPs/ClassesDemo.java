f5deb4a9a87a66f2f46b6b0109c0be2b986bbd79
yash mistry <mistryy37@gmail.com>
2022-10-20 10:39:12 +0530

    oops

---
+++ DS_ALGO/OOPs/ClassesDemo.java
@@ -0,0 +1,71 @@
+package OOPs;
+
+public class ClassesDemo {
+    public static void main(String[] args) {
+        // classses and objects
+
+       /*classses
+            classes are named group of functions and properties
+            they are user defined datatype
+            they are template to make objects 
+            they are static and not implemented an does not take any space
+            they are just rules and concept
+
+        Objects
+            initantiated class is know as object
+            it is created from class template
+            objects are created dyanmically at the runtime
+            objects store data and hence occupy space in memory
+
+        this keyword
+            'this' keyword defines what objects's property or funtions is called
+        '.' dot keyword
+            '.' keyword is used to access function or property of classes or objects
+        Constructor
+            constructors are type of special function used to initialize or create an object
+            constructors can be overloaded wiht parameters
+        
+
+
+        *    
+        */
+        Cat snowbell = new Cat();
+        // Dog rufus = new Dog();
+
+        Dog fluffy = new Dog(snowbell);
+        System.out.println(fluffy.sound);
+        System.out.println(fluffy.habitat);
+        System.out.println(fluffy.species_name);
+
+
+        // System.out.println(snowbell.habitat);
+        // System.out.println(rufus.sound);
+    }
+   
+}
+class Cat{
+    String species_name;
+    String habitat ;
+    String sound;
+    Cat(){
+        this.habitat = "wild+domestic";
+        this.sound = "meow..";
+        this.species_name = "felines";
+    }
+
+}
+class Dog{
+    String species_name ;
+    String habitat;
+    String sound;
+    Dog(){
+        this.habitat = "wild+domestic";
+        this.sound = "woofff..";
+        this.species_name = "canines";
+    }
+    Dog (Cat cat){
+        this.habitat = cat.habitat;
+        this.sound = cat.sound;
+        this.species_name = cat.species_name;
+    }
+}
